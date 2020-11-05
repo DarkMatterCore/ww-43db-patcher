ee35b66c8de1a54e2e6a5823cf017c225dc5421d
urmum-69 <urmum69@snopyta.org>
2020-11-05 21:34:18 +0000

    add 43db contents

---
+++ source/43db.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef __43DB_H__
+#define __43DB_H__
+
+extern static const unsigned char g_discdb_contents[];
+extern static const unsigned char g_vcadb_contents[];
+extern static const unsigned char g_wwdb_contents[];
+
+#endif /* __43DB_H__ */
