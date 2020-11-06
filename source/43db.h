0cb0c4ac2c91b3d4f71b4d3e978d2ecc95c20b87
urmum-69 <urmum69@snopyta.org>
2020-11-06 18:07:52 +0000

    Change type of 43db arrays to u32

--- source/43db.h
+++ source/43db.h
@@ -3,8 +3,8 @@
 #ifndef __43DB_H__
 #define __43DB_H__
 
-extern static const unsigned char g_discdb_contents[];
-extern static const unsigned char g_vcadb_contents[];
-extern static const unsigned char g_wwdb_contents[];
+extern const u32 g_discdb_contents[];
+extern const u32 g_vcadb_contents[];
+extern const u32 g_wwdb_contents[];
 
 #endif /* __43DB_H__ */
